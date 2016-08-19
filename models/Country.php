ab4ea42aa123c15d828a7623cd9a8f69f7056d68
Konovalenko Aleksandr <konovalenko@seobility.by>
2016-08-19 12:44:42 +0300

    Создал страничку с падинатором и выводом данных из БД

---
+++ models/Country.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: konovalenko
+ * Date: 18.08.2016
+ * Time: 16:13
+ */
+
+namespace app\models;
+
+use yii\db\ActiveRecord;
+
+class Country extends ActiveRecord
+{
+}
